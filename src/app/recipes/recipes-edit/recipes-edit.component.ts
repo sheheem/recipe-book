d75734db2d6288ca7dcfe3e96f5d7a9f2427e0f8
sheheem <sheheemzainudeen@gmail.com>
2023-11-18 18:17:16 +0530

    edit snapshot added

--- src/app/recipes/recipes-edit/recipes-edit.component.ts
+++ src/app/recipes/recipes-edit/recipes-edit.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-edit',
   templateUrl: './recipes-edit.component.html',
   styleUrls: ['./recipes-edit.component.css']
 })
-export class RecipesEditComponent {
+export class RecipesEditComponent implements OnInit {
+  id: number;
+  editMode = false;
+
+  constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+      this._route.params.subscribe((params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null;
+        console.log(this.editMode);
+        
+      })
+  }
 
 }
