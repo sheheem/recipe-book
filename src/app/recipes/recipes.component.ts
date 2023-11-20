2eb4a00c3318793c6dcf4371a93e3ebb25c27875
sheheem <sheheemzainudeen@gmail.com>
2023-11-20 17:33:26 +0530

    code cleaned

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -9,13 +9,11 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit{
-  selectedRecipe: Recipe;
+  
 
   constructor(private _recipeService: RecipeService) {}
 
   ngOnInit(): void {
-      this._recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      })
+     
   }
 }
